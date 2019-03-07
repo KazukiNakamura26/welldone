df8a054f39fe407e7a343bfdc41696ccb89efd48
Kazuki Nakamura <kazuki.nakamura@tcmobile.jp>
2019-03-07 18:21:21 +0900

    heroku deploy

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("chaiexpress");
+
+const port = process.env.PORT || 8000;
+
+const app = express()
+
+app.use(express.static(__dirname + "/dist/"));
+
+app.get(/.*/, function (req, res) {
+  res.sendfile(__dirname + "/dist/index.html")
+});
+
+app.listen(port);
+
+
+console.log("Server is up!!");
